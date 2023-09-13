0a7f32c0e2db2602cc562d6ae7f4b7df23f65ce0
Futtt <144862301+Futtt@users.noreply.github.com>
2023-09-13 08:42:43 +0700

    Add files via upload

---
+++ ++day1.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int h(uint32_t n) {
+    int count = 0;
+
+    while(n > 0){
+        n = n & (n - 1); 
+        count++;
+    }
+    return count;
+}
+	
+int main() {
+	vector<string> strs = {"flower","flow","flight"};
+	int n = strs.size();
+	int x = INT_MAX;
+	for (int i = 0; i < n; i++)
+        {
+            x = (x < strs[i].size()) ? x : strs[i].size();
+        }
+        cout << x;
+        bool flag = false;
+        int x1 = x;
+        int count = 0;
+        for (int k = 0; k <= x1; k++)
+        {
+        	char c = strs[0][k];
+        	
+	for (int i = 0; i < strs.size(); i++)
+	{
+		if (c != strs[i][k])
+		{
+			cout << " het" ;
+			break;
+		}
+		else {
+			flag = true;
+		}
+	}
+	if (flag == true)
+	count ++;
+	else 
+	break;
+}
+cout << count;
+    }
+
+
+    
